ed169e978cbae8f626e4ee1a2e80376c24771dc2
Anne-G <ang.schall@gmail.com>
2022-04-19 17:02:21 +0200

    step 7

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 function _enleverLeDelimiteurCustom(chaineDeNombres) {
-  const delimiteur = chaineDeNombres[2];
-  const chaineDeNombresSansDelimiteur = chaineDeNombres.slice(4);
-  return chaineDeNombresSansDelimiteur.replace(delimiteur, ',');
+  const indexDeLaFinDuDelimiteur = chaineDeNombres.indexOf(']');
+  const leDelimiteurEstMultiple = indexDeLaFinDuDelimiteur !== -1;
+
+  let delimiteur;
+  if(leDelimiteurEstMultiple) {
+    delimiteur = chaineDeNombres.substring(3, indexDeLaFinDuDelimiteur);
+  } else {
+    delimiteur = chaineDeNombres[2];
+  }
+  const chaineDeNombresSansDelimiteur = chaineDeNombres.replace(/\/\/\.*\n/, '');
+  return chaineDeNombresSansDelimiteur.replaceAll(delimiteur, ',');
 }
 
 function _verifierSilYADesNombresNegatifs(tableauDeNombres) {
@@ -48,19 +56,19 @@ function ajout(chaineDeNombres) {
 console.log(ajout("")); // 0
 console.log(ajout("1,2")); // 3
 
-// // ETAPE 2
-// // ajout() peut prendre plus que 2 nombres en entrée
+// ETAPE 2
+// ajout() peut prendre plus que 2 nombres en entrée
 console.log(ajout("1,2,1,6")); // 10
 
-// // ETAPE 3
-// // ajout() peut prendre 2 nombres séparés par des virgules OU des retours à la ligne (\n)
+// ETAPE 3
+// ajout() peut prendre 2 nombres séparés par des virgules OU des retours à la ligne (\n)
 console.log(ajout("1\n2,3")) // 6
 // console.log(ajout("1,\n")) // ERROR
 
-// // ETAPE 4
-// // ajout() supporte différents délimiteurs
-// // pour changer le délimiteur, la chaine en paramètre, suivra le pattern : "//[delimiter]\n[numbers…]"
-// // tous les anciens scénarios doivent être supportés
+// ETAPE 4
+// ajout() supporte différents délimiteurs
+// pour changer le délimiteur, la chaine en paramètre, suivra le pattern : "//[delimiter]\n[numbers…]"
+// tous les anciens scénarios doivent être supportés
 console.log(ajout("//;\n1;2")) // 3
 console.log(ajout("//;\n1;2\n3")) // 6
 
@@ -69,13 +77,13 @@ console.log(ajout("//;\n1;2\n3")) // 6
 // et afficher le / les nombres négatifs en question
 // console.log(ajout("-1,5\n-3")) // Les nombres négatifs ne sont pas autorisés : -1, -3
 
-// // ETAPE 6
-// // Les nombres plus grands que 1000 (compris) doivent être ignorés
+// ETAPE 6
+// Les nombres plus grands que 1000 (compris) doivent être ignorés
 console.log(ajout("2,1000,3")) // 5
 
-// // ETAPE 7
-// // Les délimiteurs peuvent être de n'importe quelle taille et suivent le format : "//[delimiter]\n"
-// console.log(ajout("//[***]\n1***2***3")) // 6
+// ETAPE 7
+// Les délimiteurs peuvent être de n'importe quelle taille et suivent le format : "//[delimiter]\n"
+console.log(ajout("//[***]\n1***2***3")) // 6
 
 // // ETAPE 8
 // // Autoriser les délimiteurs multiples en suivant le pattern : "//[delim1][delim2]\n"
