aecb31dc098efec5ffdf9999f3b1b531252c1d71
Marie-Hélène <marie-helene.rots@pix.fr>
2022-11-15 11:20:00 +0100

    should return sum of bowling turns

--- test/bowling_test.js
+++ test/bowling_test.js
@@ -2,17 +2,20 @@ const { expect } = require('chai');
 
 let score = (value) => {
 
-    return value;
+    return value.reduce((acc,value)=> {
+        return acc + value;
+    });
 }
 
 it('should return a score of 9', function() {
-    expect(score(9)).to.equal(9);
+    expect(score([9])).to.equal(9);
 });
 
 it('should return a score of 8', function() {
-    expect(score(8)).to.equal(8);
+    expect(score([8])).to.equal(8);
 });
 
 it('should return a sum of bowling turns', function() {
-    expect(score([8,4])).to.equal(12);
+    const thowsList = [8, 0, 4, 0, 0, 0, 0, 0, 0, 0]
+    expect(score(thowsList)).to.equal(12);
 });
