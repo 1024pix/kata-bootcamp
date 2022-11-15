915af91f72c8ccc443f247d1a4b9e1f5037900c7
Quentin Lebouc <quentin.lebouc@pix.fr>
2022-11-15 11:03:02 +0100

    should return a given value

--- test/bowling_test.js
+++ test/bowling_test.js
@@ -1,7 +1,13 @@
 const { expect } = require('chai');
 
-let score = 9;
+let score = (value) => {
+    return value;
+}
 
 it('should return a score of 9', function() {
-    expect(score).to.equal(9);
+    expect(score(9)).to.equal(9);
+});
+
+it('should return a score of 8', function() {
+    expect(score(8)).to.equal(8);
 });
